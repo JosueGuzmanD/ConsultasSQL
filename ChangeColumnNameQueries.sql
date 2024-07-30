d325b40b6e57f1ff7b5b5f4cbfde23662031a683
Robinson Josué Guzmán David <josue.guzman@semicrol.com>
2024-07-30 14:22:17 +0200

    14:22

---
+++ ChangeColumnNameQueries.sql
@@ -0,0 +1,8 @@
+exec sp_rename 'Empleados.nombre','NOMBRE';
+exec sp_rename 'Empleados.apellido','APELLIDO';
+exec sp_rename 'Empleados.edad','EDAD';
+exec sp_rename 'Empleados.telefono','TELEFONO';
+exec sp_rename 'Empleados.direccion','DIRECCION';
+exec sp_rename 'Empleados.fecha_nacimiento','FECHA_NACIMIENTO';
+exec sp_rename 'Empleados.salario','SALARIO';
+exec sp_rename 'Empleados.activo','ACTIVO';
---
+++ RelationalOperatorsQueries.sql
@@ -0,0 +1,23 @@
+select * from Empleados where nombre = 'José';
+
+select * from Empleados where edad <> 35;
+
+select * from Empleados where edad != 35;
+
+select * from Empleados where salario >2000;
+
+select nombre from Empleados where salario < 11234
+
+select nombre,edad from Empleados where salario >= 14
+
+= --Compara si dos valores son iguales
+
+<>, != --Compara si dos valores son diferentes.
+
+> --Compara si un valor es mayor que otro.
+
+< --Compara si un valor es menor que otro.
+
+ <= --Compara si un valor es menor o igual que otro.
+
+ >= --Compara si un valor es mayor o igual que otro.

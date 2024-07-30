48a4fc676bf2ad5b01c9fddb0766947d562aa49c
Robinson Josué Guzmán David <josue.guzman@semicrol.com>
2024-07-30 12:12:02 +0200

    Change Table Name, DropTable, Where Clause scripts

---
+++ DropTableQuery.sql
@@ -0,0 +1,7 @@
+create database Prueba2;
+use Prueba2;
+
+create table prueba(
+campo int);
+
+drop table prueba;
---
+++ WhereClauseQueries.sql
@@ -0,0 +1,5 @@
+select * from Empleados where nombre = 'José'
+
+select * from Empleados where edad <29
+
+select nombre, edad from Empleados where nombre = 'José'
